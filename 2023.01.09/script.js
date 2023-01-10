89dad476dd7a1907cc0315a195f7cf5576b5b785
Clare's MacBook <clare@ClaresucBookAir.T-mobile.com>
2023-01-10 22:31:04 +0900

    add: 변수이름지정규칙

--- 2023.01.09/script.js
+++ 2023.01.09/script.js
@@ -1,5 +1,17 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let PI = 3.1415;
+
+let myFirstJob = "Programer";
+let myCurrentJob = "Teacher";
