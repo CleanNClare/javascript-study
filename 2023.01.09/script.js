31f24b1878af31830ed4833c194bad31e1829146
CleanNClare <yxxnjxxxx@gmail.com>
2023-01-14 22:16:51 +0900

    add: 2023.01.14_dataTypes

--- 2023.01.09/script.js
+++ 2023.01.09/script.js
@@ -1,4 +1,9 @@
-let js = 'amazing';
+/*
+
+// Variable name conventions
+
+
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
@@ -15,3 +20,29 @@ let PI = 3.1415;
 
 let myFirstJob = "Programer";
 let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+*/
+
+//Data Types
+
+//boolean을 변수에 저장할 수도 있다.
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
+
+//변수 재할당 (값을 재할당 할때는 let을 사용하지 않는다.)
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+//undefined
+let year;
+console.log(year);
+console.log(typeof yaer);
+//undefined -> 값이 있는것으로 타입 재설정
+year = 1991;
+console.log(typeof yaer);
