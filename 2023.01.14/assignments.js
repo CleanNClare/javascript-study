0ca98d5b3b42335657aff1f02527a070910b72bd
CleanNClare <yxxnjxxxx@gmail.com>
2023-01-15 21:41:17 +0900

    add: assignments 20230115

--- 2023.01.14/assignments.js
+++ 2023.01.14/assignments.js
@@ -1,3 +1,4 @@
+/*
 //LECTURE : Values and Variables
 let country = "Korea";
 let continent = "Asia";
@@ -5,3 +6,23 @@ let population = 51;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//LECTURE : Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+
+//LECTURE : let, const and var
+language = "Korean";
+const country = "Korea";
+const continent = "Asia";
+const isIsland = false;
+//isIsland = true;
+console.log(language);
+console.log(country);
+console.log(continent);
+console.log(typeof isIsland);
