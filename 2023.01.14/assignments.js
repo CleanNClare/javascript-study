829fd650f2d6828d003bd3cf30d56db29da8167e
CleanNClare <yxxnjxxxx@gmail.com>
2023-01-29 15:20:47 +0900

    add: Basic Operators

--- 2023.01.14/assignments.js
+++ 2023.01.14/assignments.js
@@ -1,20 +1,18 @@
-/*
 //LECTURE : Values and Variables
-let country = "Korea";
-let continent = "Asia";
+//let country = "Korea";
+//let continent = "Asia";
 let population = 51;
 console.log(country);
 console.log(continent);
 console.log(population);
 
 //LECTURE : Data Types
-let isIsland = false;
-let language;
+//let isIsland = false;
+//let language;
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
-*/
 
 //LECTURE : let, const and var
 language = "Korean";
@@ -26,3 +24,19 @@ console.log(language);
 console.log(country);
 console.log(continent);
 console.log(typeof isIsland);
+
+//LECTURE : Basic Operators
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description =
+  country +
+  "is in" +
+  continent +
+  ",and its" +
+  population +
+  "million people speak" +
+  language;
+console.log(description);
